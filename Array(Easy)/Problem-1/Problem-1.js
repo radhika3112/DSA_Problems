cfb0535bee45446fd150c2b41f2a97d4020bfbd4
Dixit kumar <dxtt22@gmail.com>
2023-06-27 21:45:27 +0530

    array problems

---
+++ Array(Easy)/Problem-1/Problem-1.js
@@ -0,0 +1,16 @@
+// Ques:- Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+
+// A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+
+// Soln:-
+
+let nums = [0,2,1,5,3,4];
+function buildArray(nums) {
+    let output=[];
+    for(let i=0;i<nums.length;i++){
+        output.push(nums[nums[i]])
+    }
+    return output
+};
+
+console.log(buildArray(nums))
