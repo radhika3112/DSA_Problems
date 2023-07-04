4ef08750993c5375281e198d017fbed6c1852129
Dixit kumar <dxtt22@gmail.com>
2023-07-04 21:38:24 +0530

    problem of array

--- Array(Easy)/Problem-23/Problem-23.js
+++ Array(Easy)/Problem-23/Problem-23.js
@@ -1,1 +1,27 @@
-Ques:-
+// Ques:-Given a square matrix mat, return the sum of the matrix diagonals.
+
+// Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
+
+// Soln:-
+
+let mat = [[1,2,3],
+              [4,5,6],
+              [7,8,9]];
+
+    function diagonalSum(mat) {
+    let sum=0;
+    let n=mat.length
+for(let i=0;i<mat.length;i++){
+    for(let j=0;j<mat.length;j++){
+        if(i===j){
+            sum+=mat[i][j];
+        }
+        if(i==(n-j-1) && i!==j){
+            sum+=mat[i][j]
+        }
+    }
+}
+return sum
+};
+
+console.log(diagonalSum(mat))
