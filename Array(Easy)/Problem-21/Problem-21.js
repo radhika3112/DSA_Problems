add435d0b256814c81369f6e048c0c9fe37ce931
Dixit kumar <dxtt22@gmail.com>
2023-07-04 21:33:39 +0530

    next problem of array

---
+++ Array(Easy)/Problem-21/Problem-21.js
@@ -0,0 +1,32 @@
+// Ques:-You are given a 0-indexed, strictly increasing integer array nums and a positive integer diff. A triplet (i, j, k) is an arithmetic triplet if the following conditions are met:
+
+// i < j < k,
+// nums[j] - nums[i] == diff, and
+// nums[k] - nums[j] == diff.
+// Return the number of unique arithmetic triplets.
+
+// Soln:-
+
+let nums = [0,1,4,6,7,10];
+let diff = 3;
+
+function arithmeticTriplets(nums, diff) {
+    let output=0;
+    for(let i=0;i<nums.length;i++){
+        let num=nums[i];
+        let count=0;
+        for(let j=i+1;j<nums.length;j++){
+            if(nums[j]-num===diff){
+                count++;
+                num=nums[j];
+            }
+            if(count===2){
+                output++;
+                break;
+            }
+        }
+    }
+    return output
+};
+
+console.log(arithmeticTriplets(nums, diff))
