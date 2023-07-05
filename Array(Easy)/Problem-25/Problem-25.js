457f1e348f68e11b32875c9dda0e0ee8bbde21b9
Dixit kumar <dxtt22@gmail.com>
2023-07-05 20:43:15 +0530

    next problem of array

---
+++ Array(Easy)/Problem-25/Problem-25.js
@@ -0,0 +1,75 @@
+// Ques:- International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+
+// 'a' maps to ".-",
+// 'b' maps to "-...",
+// 'c' maps to "-.-.", and so on.
+// For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+// [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+// Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+
+// For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such a concatenation the transformation of a word.
+// Return the number of different transformations among all words we have.
+
+// Soln:-
+
+let words = ["gin","zen","gig","msg"];
+function uniqueMorseRepresentations(words) {
+     if(words.length < 2){
+        return 1;
+       } 
+    
+    let obj = {};
+    let array = [];
+    let count = 0;
+    let MorseObj = {
+        "a": ".-", 
+        "b": "-...", 
+        "c": "-.-.", 
+        "d": "-..", 
+        "e": ".", 
+        "f": "..-.", 
+        "g": "--.", 
+        "h": "....", 
+        "i": "..", 
+        "j": ".---", 
+        "k": "-.-", 
+        "l": ".-..", 
+        "m": "--", 
+        "n": "-.", 
+        "o": "---", 
+        "p": ".--.", 
+        "q": "--.-",
+        "r": ".-.",
+        "s": "...", 
+        "t": "-", 
+        "u": "..-", 
+        "v": "...-", 
+        "w": ".--",
+        "x": "-..-", 
+        "y": "-.--", 
+        "z": "--.."
+    }; 
+    
+    for(let i = 0; i < words.length; i++){
+        let result = "";
+        for(let j = 0; j < words[i].length; j++){
+                result += MorseObj[words[i][j]];
+        }
+        array.push(result);
+    }
+    
+    for (code of array){
+        if(obj[code] === undefined){
+            obj[code] = 1;
+            count++;
+           } else{
+               obj[code]+= 1;
+           }
+    }
+    
+    return count;
+ 
+};
+
+console.log(uniqueMorseRepresentations(words))
