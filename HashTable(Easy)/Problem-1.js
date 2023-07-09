f115dd39a56d5b3abee5ecab851c21eddd751925
Dixit kumar <dxtt22@gmail.com>
2023-07-09 23:16:43 +0530

    first object problem

--- HashTable(Easy)/Problem-1.js
+++ HashTable(Easy)/Problem-1.js
@@ -1,1 +1,24 @@
+// Ques:- You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
 
+// Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+// Soln:-
+
+
+let jewels = "aA";
+let stones = "aAAbbbb"
+
+
+function numJewelsInStones(jewels, stones) {
+    let count=0;
+    for(let i=0;i<jewels.length;i++){
+        for(let j=0;j<stones.length;j++){
+            if(jewels[i]===stones[j]){
+                count++;
+            }
+        }
+    }
+    return count;
+};
+
+console.log(numJewelsInStones(jewels, stones))
